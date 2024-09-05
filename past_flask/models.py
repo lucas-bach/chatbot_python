c492bcdc2c13ecd0952343588ee4298ce6b8ddcc
lucas-bach <lborgesbach@gmail.com>
2024-09-05 17:22:26 -0300

    password

--- past_flask/models.py
+++ past_flask/models.py
@@ -1,22 +1,39 @@
 from extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(200))
     done = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def to_dict(self):
         return {
             'id': self.id,
             'title': self.title,
             'description': self.description,
-            'done': self.done
+            'done': self.done,
+            'user_id': self.user_id
         }
     
+
+    
 class User(db.Model):
-    id = db.Column(db.Integer, primaty_key=True)
-    username = db.column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     tasks = db.relationship('Task', backref='user', lazy=True)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
